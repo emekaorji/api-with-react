83f0eaf5bf9d0215c651ecc346c0f92ebac89620
Emeka Orji <emekapraiseo@gmail.com>
2022-02-26 12:24:39 +0100

    Used API with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class FetchUser extends Component {
+	state = {
+		person: null,
+		picture: null,
+	};
+
+	async componentDidMount() {
+		const url = "https://api.randomuser.me/";
+		const response = await fetch(url);
+		const data = await response.json();
+		this.setState({
+			person: data.results[0],
+			picture: data.results[0].picture.large,
+		});
+	}
+
+	render() {
+		return (
+			<div className='App'>
+        {console.log(Boolean(this.state.picture))}
+				{!this.state.picture ? (
+					<div>Loading...</div>
+				) : (
+					<div>
+						{this.state.person.name.title} {this.state.person.name.first} {this.state.person.name.last}
+						<div>
+							<img src={this.state.person.picture.large} />
+              <div>oiasfdnhsoiundhfuy9hi</div>
+						</div>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
-
-export default App;
